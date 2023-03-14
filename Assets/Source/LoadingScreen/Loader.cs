c0e276de54bc8be9cdf9163c6cbd201a403dcee7
Sako <78461130+Sakooooo@users.noreply.github.com>
2023-03-14 17:36:15 +0400

    debug thing

--- Assets/Source/LoadingScreen/Loader.cs
+++ Assets/Source/LoadingScreen/Loader.cs
@@ -10,6 +10,12 @@ public class Loader : MonoBehaviour
 // https://docs.unity3d.com/ScriptReference/SceneManagement.SceneManager.LoadSceneAsync.html
   void Update()
   {
+    if (UnityEngine.Debug.isDebugBuild)
+    {
+      Debug.Log("Source/Core/Loader.cs: Starting Asynchronous Scene Load For " + name);
+    }
+
+
     StartCoroutine(LoadSceneAsync());
   }
 
